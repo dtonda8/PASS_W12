e47fc4c11aec0e2949699fe872937bcc35ef7299
dtonda8 <davidbatonda7@gmail.com>
2024-10-15 21:06:39 +1100

    Add sols

--- Q1.py
+++ Q1.py
@@ -1,7 +1,11 @@
 from typing import List
+from  data_structures.heap import MaxHeap
 
 def findKthLargest(nums: List[int], k) -> int:
-	raise NotImplementedError()
+	heap = MaxHeap.heapify(nums)
+	for _ in range(k-1):
+		heap.get_max()
+	return heap.get_max()
 
 if __name__ == "__main__":
 	pass
